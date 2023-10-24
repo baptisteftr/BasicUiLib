21ffe20a9fec18ae61d60f8895fc4c25f005e8e8
Baptiste Fortier <baptistefrtr@gmail.com>
2023-10-24 11:28:08 +0200

    Init

--- Sources/BasicUiLib/BasicUiLib.swift
+++ Sources/BasicUiLib/BasicUiLib.swift
@@ -1,2 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+@available(iOS 15.0, *)
+extension View {
+    func roundedCard(color: Color?, radius: Double?) -> some View {
+        self.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: radius != nil ? radius! : 25.0)
+                    .foregroundStyle(color != nil ? color! : Color(uiColor: .secondarySystemBackground))
+            )
+    }
+}
